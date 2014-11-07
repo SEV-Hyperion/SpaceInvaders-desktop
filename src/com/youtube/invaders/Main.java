dcc95b2e042615ef772fa910d753d3728976deb8
DaniLebredo <danilebredo@gmail.com>
2014-11-08 00:04:46 +0100

    -Added some assets already at -android.

--- src/com/youtube/invaders/Main.java
+++ src/com/youtube/invaders/Main.java
@@ -10,31 +10,33 @@ import com.badlogic.gdx.tools.imagepacker.TexturePacker2.Settings;
 import com.youtube.invaders.MainGame;
 
 public class Main {
-	
-	
-	
+
 	public static void main(String[] args) {
 		// ==================================
 		// Texture atlas Management
 		// ==================================
-		
-		if(MainGame.rebuildAtlas){
-			 Settings settings = new Settings();
-			 settings.maxWidth = 1024;
-			 settings.maxHeight = 1024;
-			 settings.debug = MainGame.drawDebugOutline;  // Set  on/off  debug texture lines
-//			 TexturePacker2.process(settings,"assets-raw/images", "../SpaceInvaders-android/assets/images","spaceinvaders.pack");// ORIGINAL
-			 TexturePacker2.process(settings,"../SpaceInvaders-android/assets-raw/images", "../SpaceInvaders-android/assets/images","spaceinvaders.pack");
+
+		if (MainGame.rebuildAtlas) {
+			Settings settings = new Settings();
+			settings.maxWidth = 1024;
+			settings.maxHeight = 1024;
+			settings.debug = MainGame.drawDebugOutline; // Set on/off debug
+														// texture lines
+			// TexturePacker2.process(settings,"assets-raw/images",
+			// "../SpaceInvaders-android/assets/images","spaceinvaders.pack");//
+			// ORIGINAL
+			TexturePacker2.process(settings,
+					"../SpaceInvaders-android/assets-raw/images",
+					"../SpaceInvaders-android/assets/images",
+					"spaceinvaders.pack");
 		}
-		
-		
-		
+
 		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
 		cfg.title = "SpaceInvaders";
 		cfg.useGL20 = true;
 		cfg.width = MainGame.WIDTH;
 		cfg.height = MainGame.HEIGHT;
-		
+
 		new LwjglApplication(new MainGame(), cfg);
 	}
 }
